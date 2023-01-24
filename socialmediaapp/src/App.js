3b35db4e78ef7c8935435993bc1ab3e31a0db90c
Piyush <piyushk123umar@gmail.com>
2023-01-24 20:42:59 +0530

    Added register page

--- socialmediaapp/src/App.js
+++ socialmediaapp/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import Login from './pages/login/Login';
-
+import Register from './pages/register/Register';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "/register",
+      element: <Register/>,
+    }
+  ]);
   return (
-    <Login/>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
