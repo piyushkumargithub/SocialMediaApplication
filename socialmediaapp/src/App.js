4d2d8cec0097d5727ff5f53a5dd4109748ba8916
Piyush <piyushk123umar@gmail.com>
2023-02-06 14:22:27 +0530

    left bar created

--- socialmediaapp/src/App.js
+++ socialmediaapp/src/App.js
@@ -21,7 +21,9 @@ function App() {
         <NavBar />
         <div style={{ display: "flex" }}>
           <LeftBar />
+          <div style={{flex:6}}>
           <Outlet />
+          </div>
           <RightBar />
         </div>
       </div>
