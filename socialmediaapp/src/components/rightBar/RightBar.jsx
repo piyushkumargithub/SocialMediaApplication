b67e26e80237e4f6855b861ff0b0ce47dfbc25d3
Piyush <piyushk123umar@gmail.com>
2023-02-07 14:04:04 +0530

    created right bar

--- socialmediaapp/src/components/rightBar/RightBar.jsx
+++ socialmediaapp/src/components/rightBar/RightBar.jsx
@@ -1,11 +1,113 @@
-import "./rightBar.scss"
-
-
+import "./rightBar.scss";
 
 const RightBar = () => {
   return (
-    <div>RightBar</div>
-  )
-}
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestion for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <span>Some User</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <span>Some other User</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <p>
+                <span>Some User</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <p>
+                <span>Some User</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <p>
+                <span>Some User</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Some User</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Some User</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg"
+                alt=""
+              />
+              <div className="online" />
+              <span>Some User</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default RightBar
+export default RightBar;
